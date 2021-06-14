991620d69666a9fee44c63851856dc719f476f04
thimiranirmal <nirmalthimira@gmail.com>
2021-06-14 22:52:48 +0530

    UI Design

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task | Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" integrity="undefined" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h1>Daily Task App</h1>
+                <form action="" method="post">
+                    <input type="text">
+                    <form action="">
+                        <table border:1px>
+                            <tr>
+                                <th>No</th>
+                                <th>Task</th>
+                                <th>is Completed</th>
+                                <th>Action</th>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>I have to sleep</td>
+                                <td>Not yet</td>
+                                <td><button class="btn-primary" type="Submit">Mark as Completed</button></td>
+                            </tr>
+                        </table>
+                    </form>
+                </form>
+            </div>
+        </div>
+        
+    </div>
+
+</body>
+</html>
