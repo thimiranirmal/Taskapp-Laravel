d0549957fb4be707db462c0018a34226b328369d
thimiranirmal <nirmalthimira@gmail.com>
2021-06-15 14:45:59 +0530

    validation text field

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,6 +9,11 @@ class TaskController extends Controller
 {
     public function store(Request $request){
         
+        $this->validate($request,[
+            'task'=>'required|min:5|max:200',
+        ]);
+
+
         $task= new Task;
         $task->task=$request->task;
         $task->save();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,11 @@
                         <form method="post" action="/saveTask" >
                         {{csrf_field()}}
                             <input type="text" name="task"  class="form-control" placeholder="Enter your Task"><br>
+                            @foreach($errors->all() as $error)
+                            <div class="alert alert-danger" role="alert">
+                            {{$error}}
+                            </div>
+                            @endforeach
                             <input type="submit" class="btn btn-primary" value="Save">
                             <input type="button" class="btn btn-warning" value="Clear">
                         </form>
